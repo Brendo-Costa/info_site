ff32cc617ac0286b71b60f1c9694656d324e7c61
Brendo-Costa <aldenir.brendo@gmail.com>
2023-10-26 19:11:40 -0300

    feat: create a style.css file to build styles for components

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"Index JS page, using to maket index." 
+
+import React  from "react";
+import  ReactDOM  from "react";
+import "./style.css"
+
+ReactDOM.render(
+    <h1>Helo Word!</h1> ,
+    document.getElementById("root")
+)
